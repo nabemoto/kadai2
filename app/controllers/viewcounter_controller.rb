06291289bac43e3dccbbf3e8e5710a60d40cd1e7
nabemoto <nabemotoaya@nabehon-no-Air.lan>
2015-12-23 22:21:15 +0900

    commit

--- app/controllers/viewcounter_controller.rb
+++ app/controllers/viewcounter_controller.rb
@@ -1,11 +1,14 @@
 class ViewcounterController < ApplicationController
 
-  def count
+  def counter
     url = params[:fullURL]
     Viewcounter.create(site_url: url)
+    render text:'Ok'.to_json
   end
 
   def index
-    
-  end
+    @total_pv = Viewcounter.all.count #総PV数
+    @site = Viewcounter.all
+    @each_site = Viewcounter.group(:site_url).order('count_all desc').count #サイト毎のPV数
+    end
 end
