a9309019c4592e8a14faad809f54bba00822859a
nabemoto <nabemotoaya@nabehon-no-Air.lan>
2015-12-24 17:21:47 +0900

    add chart

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem 'less-rails' #Railsでlessを使えるようにする。Bootstrapがlessで�
 gem 'twitter-bootstrap-rails'
 gem "chartkick"
 gem 'groupdate'
+gem 'lazy_high_charts'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/controllers/viewcounter_controller.rb
+++ app/controllers/viewcounter_controller.rb
@@ -8,7 +8,9 @@ class ViewcounterController < ApplicationController
 
   def index
     @total_pv = Viewcounter.all.count #総PV数
+    @totalPvByHour = Viewcounter.all.group_by_day(:created_at, time_zone: "Tokyo").count
     @site = Viewcounter.all
     @each_site = Viewcounter.group(:site_url).order('count_all desc').count #サイト毎のPV数
-    end
+  end
+
 end
